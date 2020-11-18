7e9bdee4cc0d50369b0deaf4fc116da78e5bbb61
AndreVTavares <andretavares3@gmail.com>
2020-11-18 15:37:32 -0300

    creating a new Toast component and a timer to remove it

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
 
-const Toast: React.FC = () => {
-  return (
-    <FiAlertCircle size={20} />
-    <div>
-      <strong>{message.title}</strong>
-      {message.description && <p>{message.description}</p>}
-    </div>
-    <button onClick={() => removeToast(message.id)} type="button">
-      <FiXCircle size={18} />
-    </button>
-  )
+import { ToastMessage, useToast } from '../../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  toast: ToastMessage;
 }
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
+const ToastContainer: React.FC<ToastProps> = ({ toast }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(toast.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, toast.id]);
+
+  return (
+    <Container type={toast.type} hasDescription={!!toast.description}>
+      {icons[toast.type || 'info']}
+      <div>
+        <strong>{toast.title}</strong>
+        {toast.description && <p>{toast.description}</p>}
+      </div>
+      <button onClick={() => removeToast(toast.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default ToastContainer;
