ab2f2ac2897fc5db6b64e0e572887aa7d91784e0
AndreVTavares <andretavares3@gmail.com>
2021-01-22 16:24:45 -0300

    styling list of appointments on dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   Calendar,
+  Section,
+  Appointment,
   NextAppointment,
 } from './styles';
 import { useAuth } from '../../hooks/auth';
@@ -16,6 +18,8 @@ import { useAuth } from '../../hooks/auth';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { user, signOut } = useAuth();
 
   return (
@@ -45,7 +49,6 @@ const Dashboard: React.FC = () => {
             <span>Dia 06</span>
             <span>Segunda-feira</span>
           </p>
-
           <NextAppointment>
             <strong>Atendimento a seguir</strong>
             <div>
@@ -61,6 +64,61 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/31252029?s=460&u=f18e74c573f5755d6d266c06af0809c2ab52d526&v=4"
+                  alt="André Tavares"
+                />
+
+                <strong>André Tavares</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/31252029?s=460&u=f18e74c573f5755d6d266c06af0809c2ab52d526&v=4"
+                  alt="André Tavares"
+                />
+
+                <strong>André Tavares</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/31252029?s=460&u=f18e74c573f5755d6d266c06af0809c2ab52d526&v=4"
+                  alt="André Tavares"
+                />
+
+                <strong>André Tavares</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
